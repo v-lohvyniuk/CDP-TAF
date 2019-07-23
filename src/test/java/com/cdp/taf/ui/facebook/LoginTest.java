0a89b751f650de4bd33a5c9a2ae3c2572bc2bf37
Volodymyr_Lohvyniuk <Volodymyr_Lohvyniuk@epam.com>
2019-07-23 18:53:19 +0300

    added listener

--- src/test/java/com/cdp/taf/ui/facebook/LoginTest.java
+++ src/test/java/com/cdp/taf/ui/facebook/LoginTest.java
@@ -1,14 +1,17 @@
 package com.cdp.taf.ui.facebook;
 
+import com.cdp.taf.listeners.AllureTestListener;
 import com.cdp.taf.testbase.UiTestBase;
 import com.cdp.taf.ui.bo.LoginRegisterBO;
 import com.cdp.taf.data.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static com.cdp.taf.ui.bo.LoginRegisterBO.getSampleUser;
 import static org.testng.Assert.assertFalse;
 
+@Listeners({AllureTestListener.class})
 public class LoginTest extends UiTestBase {
 
     @Autowired
